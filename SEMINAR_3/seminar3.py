725f3a4a65b9f1d0c9402ce81e3939a0fdc8fb42
Alex Green <a_green@inbox.ru>
2022-07-14 21:31:15 +0300

    more task done

--- SEMINAR_3/seminar3.py
+++ SEMINAR_3/seminar3.py
@@ -1,25 +1,106 @@
-sp = [5, 11, 2, 3, 4, 8, 157, 555]
+import json
+sp=[5,11,2,3,4,8,157,555]
+
 def max_average(sp):
-    max = sp[0]
-    sr = 0
+    maxx=sp[0]
+    sr=0
     for i in sp:
-        if i > max:
-            max = i
-        sr += i
-    sr = sr/len(sp)
-    rez = {}
-    rez['максимальный элемент'] = max
-    rez['среднее арифмитическое'] = sr
-    k = (max, sr, rez)
+        if i > maxx:
+            maxx=i
+        sr+=i
+    sr=sr/len(sp)
+    rez={}
+    rez['максимальный элемент']=maxx
+    rez['среднее арифметическое']=sr
+    k=(maxx,sr,rez)
     return k
 
-res = max_average(sp)
+with open('data.json', 'r', encoding='utf-8') as fh:  # открываем файл на чтение
+                BD = json.load(fh)  # загружаем из файла данные в словарь data
+print('БД успещно загружена')
+print(type(BD))
+
+
+#res=max_average(sp)
+
+#with open('data.json', 'w', encoding='utf-8') as fh:  # открываем файл на запись
+    
+ #           fh.write(json.dumps(res, ensure_ascii=False))  # преобразовываем словарь data в unicode-строку и записываем в файл
+#print('БД успещно сохранена')
 #print(res[2])
-for x,y in res[2].items():
-    print(x, y)
+#for x,y in res[2].items():
+ #   print(x,y)
 
 '''
 print(len(rez))
 for x,y in rez.items():
-    print(x, y)
+    print(x,y)
+'''    
+
+
+'''Задайте список из n чисел ряда фибоначчи
+
+
+def fibo(n):
+    if n in [1,2]:
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+x = int(input('Введите число элементов: '))
+
+def fibolist(x):
+    list = []
+    for i in range(1, x+1):
+        list.append(fibo(i))
+    return list
+print(fibolist(x))
+'''
+
+'''сделать функцию, на вход принимающую список,
+ на выходе возращающая словарь, где указаны максимальное число,
+ минимальное, их индексы, и среднее арифметическое
+
+
+N = 5
+
+def inisp(N):
+    sp = [];
+    for i in range(1,N+1):
+        sp.append(round((1+1/i)**i,2))
+    return sp
+
+def inib(sp):
+    bib = {}
+    max = sp[0]
+    idmax = 0
+    min = sp[0]
+    idmin = 0
+    sum = 0
+    for i in range(len(sp)):
+        if(max<sp[i]):
+            max = sp[i]
+            idmax = i
+        if(min>sp[i]):
+            min = sp[i]
+            idmin = i
+        sum += sp[i]
+    sred = sum/len(sp)
+    bib['max'] = max
+    bib['idmax'] = idmax
+    bib['min'] = min
+    bib['idmin'] = idmin
+    bib['sred'] = sred
+    return bib
+
+spisok = inisp(N)
+bibl = {}
+print(spisok)
+bibl = inib(spisok)
+print(bibl)
+file = open('HW1.txt','a')
+for a,b in bibl.items():
+    file.write(f'{a} - {b} ')
+file.write('\n')
+file.close()
 '''
